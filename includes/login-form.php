9a5a157546b0dc5b9e36e852de8d3de28fa90188
Parvir Sandhu <p.sandhu325@mybvc.ca>
2018-04-04 14:55:37 -0600

    login page bootstrap

--- includes/login-form.php
+++ includes/login-form.php
@@ -11,47 +11,38 @@ session_start();
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="bootstrap-3.2.0-dist/css/Common.css" type="text/css" rel="stylesheet">
         <title>Login Art Store</title>
         <!-- Custom styles for this template -->
         <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Palanquin" rel="stylesheet">
         <link href="iwppa1.css" rel="stylesheet">
     </head>
-    <body>
-        <br>
-        <br>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
-                    <form class="form-horizontal" method="POST" action="login.php" id="login">
-                        <div class="form-group">
-                            <label>Email address</label>
-                            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
-                        </div>
-
-                         <div class="form-group">
-                            <label>Password</label>
-                            <input type="Password" class="form-control" name="password" id="pass" aria-describedby="passwordHelp" placeholder="Enter Password">
-                        </div>
-
-                        <div class="form-group">
-                            <label class="col-lg-5"></label>
-                            <div class="col-md-3">
-                                <button name="submit" type="submit" class="btn btn-primary" >login <span class="glyphicon glyphicon-check"></span></button>  
-                            </div>
-                        </div>
-                    </form>
+    <body style="background-color: #8a6d3b">
+    <div class="container">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+            <div class="jumbotron" style="margin-top: 200px">
+                <h2>Please Login</h2><br>
+                <form method="POST" action="login.php">
                     <div class="form-group">
-                        <label class="col-lg-5"></label>
-                        <div class="col-md-3">
-                            <a href="../iwppa2-works.php"><button class="btn btn-danger" >Cancel <span class="glyphicon glyphicon-remove-sign"></span></button></a>
-                        </div>
+                        <label>Email address</label>
+                        <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="Password" class="form-control" name="password" id="pass" aria-describedby="passwordHelp" placeholder="Enter Password">
+                    </div><br>
+                    <button name="submit" type="submit" class="btn btn-primary form-control" >Login  <span class="glyphicon glyphicon-check"></span></button><p></p>
+                    <a href="../iwppa2-works.php"><button class="btn btn-danger form-control" >Cancel <span class="glyphicon glyphicon-remove-sign"></span></button></a>
+                </form>
             </div>
         </div>
+        <div class="col-lg-4"></div>
+    </div>
 
-    </body>
+
     <script src="bootstrap-3.2.0-dist/js/jQuery.js"></script>
     <script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+    </body>
 </html>
