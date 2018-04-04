d7dac6f1b07bc84d6311a1a0fe6d3814c212e772
Parvir Sandhu <p.sandhu325@mybvc.ca>
2018-04-04 16:16:17 -0600

    Login page fixed layout

--- includes/login-form.php
+++ includes/login-form.php
@@ -11,16 +11,35 @@ session_start();
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link href="bootstrap-3.2.0-dist/css/Common.css" type="text/css" rel="stylesheet">
+        <link href="../bootstrap-3.2.0-dist/css/Common.css" type="text/css" rel="stylesheet">
         <title>Login Art Store</title>
         <!-- Custom styles for this template -->
         <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Palanquin" rel="stylesheet">
-        <link href="iwppa1.css" rel="stylesheet">
+        <link href="../iwppa1.css" rel="stylesheet">
     </head>
-    <body style="background-color: #8a6d3b">
-    <div class="container">
-        <div class="col-lg-4"></div>
+    <body class="loginbg">
+    <div class="container ">
+        <div class="row">
+            <div class="col-md-4 col-sm-4 col-xs-12"></div>
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <form class="formConatiner" method="POST" action="login.php">
+                    <h2>Please Login</h2><br>
+                    <div class="form-group">
+                        <label>Email address</label>
+                        <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="Password" class="form-control" name="password" id="pass" aria-describedby="passwordHelp" placeholder="Enter Password">
+                    </div><br>
+                    <button name="submit" type="submit" class="btn btn-primary form-control" >Login  <span class="glyphicon glyphicon-check"></span></button><p></p>
+                    <a href="../iwppa2-works.php"><button class="btn btn-danger form-control" >Cancel <span class="glyphicon glyphicon-remove-sign"></span></button></a>
+                </form>
+            </div>
+            <div class="col-md-4 col-sm-4 col-xs-12"></div>
+        </div>
+      <!--  <div class="col-lg-4"></div>
         <div class="col-lg-4">
             <div class="jumbotron" style="margin-top: 200px">
                 <h2>Please Login</h2><br>
@@ -38,11 +57,11 @@ session_start();
                 </form>
             </div>
         </div>
-        <div class="col-lg-4"></div>
+        <div class="col-lg-4"></div> -->
     </div>
 
 
-    <script src="bootstrap-3.2.0-dist/js/jQuery.js"></script>
-    <script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+    <script src="../bootstrap-3.2.0-dist/js/jQuery.js"></script>
+    <script src="../bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
     </body>
 </html>
