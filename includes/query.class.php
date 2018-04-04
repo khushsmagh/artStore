82e6840e75bb54cf7b55b64d99e4a0c181e9deea
n.beekhuijs948 <n.beekhuijs948@mybvc.ca>
2018-04-04 09:21:47 -0600

    created query class

---
+++ includes/query.class.php
@@ -0,0 +1,35 @@
+<?php
+
+class Query {
+	
+	protected $resultSet;
+	protected $resultCount;
+	
+	function __construct($qry) {
+		
+		require_once('includes/config.database.php');
+		$this->resultCount = 0;
+		try	{
+			$pdo	=	new	PDO(DBCONNSTRING,DBUSER,DBPASS);
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$result	=	$pdo->query($qry);
+			while	($row	=	$result->fetch()) {
+				$this->resultSet[] = $row;
+				$this->resultCount++;
+			}
+			$pdo	=	null;	
+		}	
+		catch	(PDOException	$e)	{
+			die(	$e->getMessage()	);
+		}		
+	}
+	
+	public function resultSet() {
+		return $this->resultSet;
+	}
+
+		public function resultCount() {
+		return $this->resultCount;
+	}
+
+}
---
+++ testqry.php
@@ -0,0 +1,23 @@
+<?php
+include('includes/header.inc.php');
+require_once('includes/query.class.php');
+
+$sql = "SELECT * 
+		  FROM paintings
+		 ORDER BY Title";
+
+$query 	= new Query($sql);
+$set 	= $query->resultSet();
+$count 	= $query->resultCount();
+?>
+        <div class="container"> <!-- main content container -->
+<?php
+		echo 'Count:'.$count.'<br><br>';
+
+		foreach($set as $row){
+		echo $row['Title'].'<br>';	
+		}
+?>		
+        </div> <!-- end main content container -->
+		
+<?php include('includes/footer.inc.php'); ?>
