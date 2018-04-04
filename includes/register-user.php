2887ec73bcf22042acb2223a19ecd6ee06ca9a7a
p.rodriguez673 <p.rodriguez673@mybvc.ca>
2018-04-04 17:43:01 -0600

    Validation for register user form added

--- includes/register-user.php
+++ includes/register-user.php
@@ -16,31 +16,62 @@ if(isset($_POST['submit'])){
 	$password = $_POST['password'];
 	$cpassword = $_POST['cpassword'];
 
+	
 	// Validate if the password is the same before registration
 	if($password != $cpassword){
-		//header("Location:register-user.form.php?Incorrect=Password");
 		echo "Please check if your password is the same";
+		exit();
 	}
 
+	// Validation for empty fields
 	if(empty($firstName) || empty($lastName) || empty($address) || empty($city) || empty($region) || empty($country) || empty($postalCode) || empty($phone) || empty($email) || empty($password) || empty($cpassword)){
-
-		//header("Location:register-user.form.php?Missing=Fields");
 		echo "missing fields";
 		exit();
 	}
 
-	/*// Checking if the user input is valid for our Database
-	if(preg_match(!"/^[a-zA-Z]*$/", $firstName) || preg_match(!"/^[a-zA-Z]*$/", $lastName)){
-		//header("Location:register-user.form.php?Invalid=Name");
-		echo "Check your name";
-		exit();
-	}  */
+	// Validate phone number
+	// Checking that this does not have letters or special characteres
+	if(preg_match("/^[0-9]{3}-[0-9]{4}-[0-9]{4}$/", $phone)) {
+  		echo "Phone number is not valid";
+  		exit();
+	}
 
-	
-	//Checking if e-mailis valid
+	// Validate lenght of the phone (American)
+	// If phone is greater than 10, is not valid
+	if (strlen($phone) > 10){
+		echo "Please enter a valid number phone";
+		exit();
+	}
+
+	// Checking if the user name input is valid
+	if(!preg_match("/^[a-zA-Z ]*$/", $firstName)){
+		echo "Invalid name";
+		exit();
+	} 
+
+	// Checking if the user lastName input is valid
+	if(!preg_match("/^[a-zA-Z ]*$/", $firstName)){
+		echo "Invalid last name";
+		exit();
+	}
+
+	//Checking if e-mail is valid
 	if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
 		//header("Location:register-user.form.php?Invalid=Email");
 		echo "Check valid email";
+		exit();
+	}
+
+	// Checking for valid postal code
+	if(!preg_match("/^([a-zA-Z]\d[a-zA-Z])\ {0,1}(\d[a-zA-Z]\d)$/", $postalCode)){
+		echo "Invalid postal code";
+		exit();
+	}
+
+	// Checking if postalCode lenght is valid
+	// if greater than 6, is not valid
+	if(strlen($postalCode) > 6){
+		echo "Invalid postal code";
 		exit();
 	}
 		
@@ -79,6 +110,7 @@ if(isset($_POST['submit'])){
 		echo "Connection succesfully";
 		exit();
 	}
+
 }
 	
 ?>
