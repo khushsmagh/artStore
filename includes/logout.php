4f572139d2c8208476cfc9958940538ede4c9c78
p.rodriguez673 <p.rodriguez673@mybvc.ca>
2018-04-04 18:46:38 -0600

    Change header location for logout page added

--- includes/logout.php
+++ includes/logout.php
@@ -6,7 +6,7 @@ if(isset($_POST['submit'])){
 	session_start();
 	session_unset();
 	session_destroy();
-	header("Location:../iwppa2-works.php?logout=succesfully");
+	header("Location:login-form.php");
 }
 
 ?>
