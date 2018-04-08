a7e9d3fb1bd1aed6c16bc3d5fd9adbaecce4ea58
k.smagh938 <k.smagh938@mybvc.ca>
2018-04-08 00:22:36 -0600

    Single artist

--- includes/view-single-artist.php
+++ includes/view-single-artist.php
@@ -1,18 +1,68 @@
 <?php
-include 'config.database.php';
+include ('config.database.php');
+include ('single-artist.class.php');
+include ('single-artist-painting.class.php');
 function ViewSingleArtist()
 {
     try {
+        $getArtistID = $_GET['ArtistID'];
         $connection = new PDO(DBCONNSTRING, DBUSER, DBPASS);
         // set the PDO error mode to exception
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $GetArtistID = $_GET['ArtistID'];
-        $sql = "Select *  , CONCAT(FirstName , ' ', LastName) AS Name from artists Where ArtistID = $GetArtistID";
+        $sql = "Select * from artists Where ArtistID = $getArtistID";
         $query = $connection->query($sql);
         $row = $query->fetch();
-        return $row;
+        $artist = new SingleArtist($row['ArtistID'] , $row['FirstName'] , $row['LastName'] , $row['Nationality'] , $row['Gender'] , $row['YearOfBirth'] , $row['YearOfDeath'] , $row['Details'] , $row['ArtistLink']);
+        return $artist;
     } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
 
+function ViewArtistPainting()
+{
+
+    try {
+        $getArtistID = $_GET['ArtistID'];
+        $connection = new PDO(DBCONNSTRING, DBUSER, DBPASS);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "select * from paintings Where ArtistID = $getArtistID";
+        $query = $connection->query($sql);
+        while ($row = $query->fetch()) {
+            $apainting = new SingleArtistPainting($row['PaintingID'], $row['ArtistID'], $row['ImageFileName'], $row['Title'], $row['ShapeID'], $row['Description'], $row['Excerpt'],
+                $row['YearOfWork'], $row['Width'], $row['Height'], $row['Medium'], $row['Cost']);
+            $painting[] = $apainting;
+        }
+        return $painting;
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
+
+
+function OutputArtistPainting()
+{
+    $getpainting = ViewArtistPainting();
+    foreach ($getpainting as $showpainting)
+    {
+       echo '
+       <div class="col-md-3">
+        <div class="thumbnail">
+       <a href = "iwppa2-works.php?PaintingID='.$showpainting->getPaintingID().'"><img class="img-thumbnail" src="images/works/square-medium/'.$showpainting->getImageFIleName().'.jpg" alt="..."></a>
+        <div>
+        <p class="similarTitle"><a href="#"><b>'.$showpainting->getPaintingTitle().'</b></a></p>
+        <button class="btn btn-primary btn-sm" type="button"><span
+                class="glyphicon glyphicon-info-sign"></span> View
+        </button>
+        <button class="btn btn-success btn-sm" type="button"><span
+                class="glyphicon glyphicon-gift"></span> Wish
+        </button>
+        <button class="btn btn-info btn-sm" type="button"><span
+                class="glyphicon glyphicon-shopping-cart"></span>Cart
+        </button>
+        </div>
+        </div>
+        </div>';
     }
 }
 ?>
--- single-artist.php
+++ single-artist.php
@@ -1,20 +1,17 @@
 <?php 
 $pageTitle = 'Artist detail';
-include('includes/header.inc.php'); 
-
-include 'includes/view-single-artist.php';
-
-$artistID = $_GET['ArtistID'];
+include ('includes/view-single-artist.php');
+include('includes/header.inc.php');
 $row = ViewSingleArtist();
 ?>
-            <h1><?php echo $row['Name']?></h1>
+            <h1><?php echo $row->getFirstname().' '.$row->getLastname()?></h1>
         <div class="row">
-            <div class="col-md-3">
-                <img src="images/artists/medium/<?php echo $row['ArtistID']?>.jpg" class="img-thumbnail img-responsive" alt="Self-portrait in a Straw Hat"/>
-            </div>
             <div class="col-md-5">
+                <img src="images/artists/medium/<?php echo $row->getArtistID()?>.jpg" class="img-thumbnail img-responsive" alt="Self-portrait in a Straw Hat"/>
+            </div>
+            <div class="col-md-6">
                 <p>
-                   <?php echo $row['Details'] ?></p>
+                   <?php echo $row->getDetails() ?></p>
                 <div class="btn-group">
                     <button class="btn btn-default" type="button" >
                         <span style="color:rgb(0, 143, 190)"><span class="glyphicon glyphicon-gift"></span> Add to Favorites List</span>
@@ -23,40 +20,43 @@ $row = ViewSingleArtist();
                 <p>&nbsp;</p>
                 <div class="panel panel-default">
                     <div class="panel-heading">Artist Details</div>
+                    <div class = "panel-body">
+                        <div id ="table_adjustment">
                     <table class="table">
                         <tr>
                             <th>Date:</th>
-                            <td><?php echo $row['YearOfBirth'] ?>-<?php echo $row['YearOfDeath']?></td>
+                            <td><?php echo $row->getYearofbirth() ?>-<?php echo $row->getYearofdeath()?></td>
                         </tr>
                         <tr>
                             <th>Nationalitly:</th>
-                            <td><?php echo $row['Nationality'] ?></td>
+                            <td><?php echo $row->getNationality()?></td>
                         </tr>
                         <tr>
                             <th>More info:</th>
-                            <td><?php echo $row['ArtistLink'] ?></td>
+                            <td><?php echo $row->getArtistlink()?></td>
                         </tr>
                     </table>
                 </div>
             </div>
+                </div>
+            </div>
             <div class="row">
                 <div class="col-md-10">
-                    <h2 style="font-family: Stoke">Art By <?php echo $row['Name'] ?></h2>
+                    <h2 style="font-family: Stoke">Art By <?php echo $row->getFirstname().' '.$row->getLastname()?></h2>
                 </div>
+            </div>
+        </div>
+</div>
                 <?php
-                $sql = "select * from paintings Where ArtistID = $artistID";
-                $query = $connection->query($sql);
-                while($result = $query->fetch()) {
+                OutputArtistPainting();
                 ?>
-
-                <div class="col-md-3">
+                <!--<div class="col-md-3">
                     <div class="panel panel-default">
                         <div class="similar-products">
 
-
-                            <a href = "iwppa2-works.php?PaintingID=<?php echo $result['PaintingID']?>"><img class="img-thumbnail" src="images/works/square-medium/<?php echo $result['ImageFileName']?>.jpg" alt="..."></a>
+                            <a href = "iwppa2-works.php?PaintingID=<?php //echo $result['PaintingID']?>"><img class="img-thumbnail" src="images/works/square-medium/<?php //echo $result['ImageFileName']?>.jpg" alt="..."></a>
                                 <div>
-                                    <p class="similarTitle"><a href="#"><b><?php echo $result['Title']?></b></a></p>
+                                    <p class="similarTitle"><a href="#"><b><?php //echo $result['Title']?></b></a></p>
                                     <button class="btn btn-primary btn-sm" type="button"><span
                                                 class="glyphicon glyphicon-info-sign"></span> View
                                     </button>
@@ -69,10 +69,10 @@ $row = ViewSingleArtist();
                                 </div>
                         </div>
                     </div>
-                </div>
-                    <?php
-                }
-                ?>
+                </div>-->
             </div>
             </div>
-<?php include('includes/footer.inc.php'); ?>
+<script src="bootstrap-3.2.0-dist/js/jQuery.js"></script>
+<script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+</body>
+</html>
