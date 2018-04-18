5dfbcc3ba96eeaa307d7be5a0018548193600781
p.rodriguez673 <p.rodriguez673@mybvc.ca>
2018-04-18 16:44:22 -0600

    Group collaboration, About Us information

---
+++ collaboration.php
@@ -0,0 +1,49 @@
+<?php 
+include("includes/header.inc.php");
+?>
+
+<div class="container">
+	<br>
+	<h1>Collaboration Group Project</h1>
+	<div class="col-md-6">
+		<h2>Pablo Winter</h2>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+            <li></li>
+			<li></li>
+				
+		</ul>
+		<h2>Khushveen Smagh</h2>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>	
+		</ul>
+		<h2>Nico Beekhuijs</h2>
+		<ul>
+			<li></li>
+			<li></li>
+            <li></li>
+			<li></li>
+            <li></li>
+			<li></li>	
+		</ul>
+		<h2>Parvir Sandhu</h2>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>	
+		</ul>
+	</div>	
+</div>
+
+<?php include("includes/footer.inc.php"); ?>
--- iwppa2-about.php
+++ iwppa2-about.php
@@ -4,9 +4,9 @@ include('includes/header.inc.php');
 ?>
             <div class="jumbotron">
                 <h2 > About Us</h2>
-                    <p> This is a hypothetical site  created as a term project by <em> Pablo Winter</em> and <em>Khushveen Smagh. </em></p>
+                    <p> This is a hypothetical site  created as a term project by <em> Pablo Winter</em>,<em>  Khushveen Smagh, </em><em> Nico Beekhuijs</em> and <em>Parvir Sandhu. </em></p>
                     <p> It was created for SODV1201 at Bow Valley College taught by Klinger Silva. </p>
-                    <button class="btn btn-primary btn-lg"> Learn more</button>
+                    <a href="collaboration.php" class="btn btn-primary btn-md"> Learn more</a>
             </div>
 
 <?php include('includes/footer.inc.php'); ?>
