6e3a3b61a643d70ccd112492f3de79a0e7dc8d98
p.rodriguez673 <p.rodriguez673@mybvc.ca>
2018-04-19 08:02:33 -0600

    Collaboration added

--- collaboration.php
+++ collaboration.php
@@ -9,40 +9,47 @@ include("includes/header.inc.php");
 	<div class="col-md-6">
 		<h2>Pablo Winter</h2>
 		<ul>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-            <li></li>
-			<li></li>
-				
+			<li>Use case Register user</li>
+			<li>Login and logout system</li>
+			<li>Php validation</li>
+			<li>Use case update user profile (admin)</li>
+			<li>Use case simple search</li>
+			<li>Use case search results</li>
+			<li>Use case update customer profile</li>
+			<li>Use case about us</li>
+			<li>Session management</li>
+			<li>CRUD operations</li>	
 		</ul>
 		<h2>Khushveen Smagh</h2>
 		<ul>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>	
+			<li>Use case home page</li>
+			<li>Use case display single artist</li>
+			<li>Use case display single art work</li>
+			<li>Use case browse genres</li>
+			<li>Single subject</li>
+			<li>Use case add reviews</li>
+			<li>View reviews</li>
+			<li>CRUD operations</li>	
 		</ul>
 		<h2>Nico Beekhuijs</h2>
 		<ul>
-			<li></li>
-			<li></li>
-            <li></li>
-			<li></li>
-            <li></li>
-			<li></li>	
+			<li>Use case Browse Galleries</li>
+			<li>Use case add to cart</li>
+			<li>Use case view cart</li>
+			<li>Use case checkout</li>
+			<li>Creation of the classes</li>
+			<li>Fixing bugs during testing</li>
+			<li>Bootstrap theme and site design</li>
+			<li>Session management</li>
+			<li>CRUD operations</li>	
 		</ul>
 		<h2>Parvir Sandhu</h2>
 		<ul>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>	
+			<li>Use case Navigation</li>
+			<li>Bootstrap theme and site design</li>
+			<li>Use case add reviews</li>
+			<li>View reviews</li>
+			<li>Website layout</li>
 		</ul>
 	</div>	
 </div>
